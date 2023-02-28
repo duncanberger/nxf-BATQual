60f505c13bb1e2d97a44c68c67263ab5482bcb7c
Duncan Berger <duncan.berger@bath.edu>
2023-02-28 17:17:17 +0000

    Update run_poppunk.sh

--- scripts/run_poppunk.sh
+++ scripts/run_poppunk.sh
@@ -39,8 +39,10 @@ do
     echo $sample_id $baseDir/$sample_id/$sample_id.velvet_contigs.fa | tr ' ' '\t'
 done < "$sample_list" > run_GPSC.list
 
+# Run Poppunk on the combined input (giving a list of all relevant accessions)
 poppunk_assign --db $baseDir/DB/GPS_v6 --distances $baseDir/DB/GPS_v6/GPS_v6.dists --query GPSC_input.txt --output out_GPSC --external-clustering $baseDir/DB/GPS_v6_external_clusters.csv --threads 5
 
+# Split the result into individual files and place them back in each samples directory
 while read -r sample_id
 do
 grep $sample_id out_GPSC_external_clusters.csv | awk -F, '{print $1,"GPSC",$2,""}' OFS=',' > $baseDir/$sample_id/$sample_id.GPSC_results.txt
