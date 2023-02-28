bbb67df810edf80e422509a0ecbae7ea82bbb682
Duncan Berger <duncan.berger@bath.edu>
2023-02-28 19:31:19 +0000

    Update run_poppunk.sh

--- scripts/run_poppunk.sh
+++ scripts/run_poppunk.sh
@@ -35,17 +35,20 @@ done
 # Check if required parameters are present
 if [ -z "$sample_list" ] || [ -z "$baseDir" ]
 then
-    echo "Missing required parameter(s). Usage: $0 --sample_list SAMPLE_LIST --baseDir BASE_DIR"
+    echo "Missing required parameter(s). Usage: $0 --sample_list SAMPLE_LIST --baseDir BASE_DIR --threads THREADS"
     exit 1
 fi
 
-# Execute code for each sample in the list
+# Create Poppunk input list (a list of sample IDs and FASTA file locations)
 while read -r sample_id
 do
     echo $sample_id $baseDir/$sample_id/$sample_id.velvet_contigs.fa | tr ' ' '\t'
 done < "$sample_list" > run_GPSC.list
 
-echo "poppunk_assign --db $baseDir/DB/GPS_v6 --distances $baseDir/DB/GPS_v6/GPS_v6.dists --query GPSC_input.txt --output out_GPSC --external-clustering $baseDir/DB/GPS_v6_external_clusters.csv --threads $threads"
+# Run Poppunk on all samples
+poppunk_assign --db $baseDir/DB/GPS_v6 --distances $baseDir/DB/GPS_v6/GPS_v6.dists --query GPSC_input.txt --output out_GPSC --external-clustering $baseDir/DB/GPS_v6_external_clusters.csv --threads $threads
+
+# Split GPSC output and write individual result files into each sample directory (for further processing by aggregate.py)
 while read -r sample_id
 do
 grep $sample_id out_GPSC_external_clusters.csv | awk -F, '{print $1,"GPSC",$2,""}' OFS=',' > $baseDir/$sample_id/$sample_id.GPSC_results.txt
